d2eae0b46e4a31619167e2fb924d36e2ff61b767
Bagorafisk <81960941+bagorafisk@users.noreply.github.com>
2025-06-14 16:27:23 +0200

    Added Expressions

---
+++ include/expr.h
@@ -0,0 +1,40 @@
+#ifndef EXPR_H
+#define EXPR_H
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <token.h>
+
+struct Expr {
+  virtual ~Expr() = default;
+};
+
+struct NumberExpr : Expr {
+  double value;
+  NumberExpr(double val) : value(val) {}
+};
+
+struct VariableExpr : Expr {
+  std::string name;
+  VariableExpr(std::string name) : name(std::move(name)) {}
+};
+
+struct BinaryExpr : Expr {
+  std::unique_ptr<Expr> left;
+  Token::Type op;
+  std::unique_ptr<Expr> right;
+
+  BinaryExpr(std::unique_ptr<Expr> l, Token::Type o, std::unique_ptr<Expr> r)
+    : left(std::move(l)), op(o), right(std::move(r)) {}
+};
+
+struct CallExpr : Expr {
+  std::string callee;
+  std::vector<std::unique_ptr<Expr>> arguments;
+
+  CallExpr(std::string callee, std::vector<<std::unique_ptr<Expr>> args)
+    : callee(std::move(callee)), arguments(std::move(args)) {}
+};
+
+#endif
